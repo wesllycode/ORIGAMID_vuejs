6177238282a0c7ff3a7e55cab267c51fb0596f1a
pwcodigo <pwcodigo@email.com>
2020-05-13 22:09:00 -0300

    feat:importando e exportanto componentes dentro de outro componentes

--- aula501-parte2/MenuAnime.js
+++ aula501-parte2/MenuAnime.js
@@ -3,19 +3,19 @@
     do MenuAnime.js
 */
 
-import MenuAnime from "./ListAnime.js";
+import ListAnime from "./ListAnime.js";
 
 export default{
     name:"MenuAnime",
     template: `
-    
-        <ul>
-            <li> Página Inicial </li>
-            <li> Animes Dublados </li>
-        </ul>
-
+       
+            <ul>
+                <li> Página Inicial </li>
+                <li> Animes Dublados </li>
+                <list-anime></list-anime>            
+            </ul>
     `,
     components: {
-        MenuAnime
+        ListAnime
     }
 }
