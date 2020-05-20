35edaf9f254b1c739308232b6651dc8cd1e859b6
pwcodigo <pwcodigo@email.com>
2020-05-20 08:30:31 -0300

    feat: Adicionei filter para inserir simbolo do celsius, puxei api com methods e fiz criar assim que vue.js inicializar com o hooks created

---
+++ aula501-parte3/Tempo.js
@@ -0,0 +1,34 @@
+export default {
+    name: "Tempo",
+    data(){
+        return {
+           tempe: ""
+        };
+    },
+    template: ` <div>
+                    <p><b>Nome da cidade</b>: {{tempe.city}}</p>
+                    <p><b>Temperatura: </b>: {{ tempe.temp | Graus}}</p>
+                    <p><b>Descrição</b>: {{tempe.description}}</p>  
+                
+                </div>`,
+    created(){
+        this.puxarApi();
+    },
+    filters:{
+        Graus(value){ 
+            value += "º"          
+            return value;
+        }
+
+    },
+    methods:{
+        puxarApi(){
+            fetch("http://api.hgbrasil.com/weather?woeid=12578813&key=3367f5b5")
+            .then(r => r.json())
+            .then(r => {
+                this.tempe = r.results;
+            } )
+        }
+    }
+    
+}
