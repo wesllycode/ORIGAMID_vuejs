ab68258869978d1562f8afc6b66f98f34439b75f
pwcodigo <pwcodigo@email.com>
2020-05-27 10:48:21 -0300

    feat: feito exemploda aula até uso do setTimeout

---
+++ aula503 - events/components/eventoBotao.js
@@ -0,0 +1,17 @@
+export default{
+    name: "eventoBotao",    
+    template: `
+        <div>
+           <button @click="handleClick">Enviar Mensagem</button>
+        </div>
+    `,
+    methods:{
+        handleClick(){            
+        }
+    },
+    created(){
+        setTimeout(() =>{
+                this.$emit("nomedoevento")
+            }, 2000)
+        },
+    }
