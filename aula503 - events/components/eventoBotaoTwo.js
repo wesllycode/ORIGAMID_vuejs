5e698957488dfd8b855ca258c0b627abfe6fbb0e
pwcodigo <pwcodigo@email.com>
2020-06-02 12:30:01 -0300

    feat: feito alteracao para comecar novo exemplo

---
+++ aula503 - events/components/eventoBotaoTwo.js
@@ -0,0 +1,12 @@
+export default{
+    name: "eventoBotaoTwo",    
+    template: `
+        <div>
+           <button>Enviar Mensagem</button>
+        </div>
+    `,    
+    methods:{
+       
+    }
+
+}
