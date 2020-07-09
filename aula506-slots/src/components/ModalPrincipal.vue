85fcfa26dc78ad64af91751710cb6e9eb8acdfee
pwcodigo <pwcodigo@email.com>
2020-07-08 21:48:44 -0300

    feat: Aula até 09:21 - resumo sobre slots e uso v-slot

---
+++ aula506-slots/src/components/ModalPrincipal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="modal">
+      <slot name="top"></slot>        
+        <h1>Esse é o modal</h1>
+            <slot></slot>
+        <slot name="footer"></slot>
+      <!-- 
+          Quando eu tenho mais de um slot eu posso renomear  
+            <div class="navbar">
+                <slot name="cabecalho"></slot>
+            </div>
+            <section>
+                mensagem que fica no meio
+            </section>
+            <div class="conteudo">
+                <slot name="container"></slot>
+            </div>
+
+
+            E para usa o slot basta utilizar
+                <template v-slot:cabecalho></template>
+      -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
